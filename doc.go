0e145c200da1cb85b7f5ca745c91d75ebaca13d2
IngCr3at1on <sblood86@gmail.com>
2016-11-01 20:31:46 -0500

    Add a basic package description for godoc and basic/main example

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package Singularity
+// A Simple Slack Client/Framework in golang. Allows you to easily hook up
+// commands and event listeners, or will at some point. Right now it is in
+// progress! Nothing really here but the writings of a mad man.
+package singularity
+
+// Add package description related comments above package declaration.
---
+++ example_main.go
@@ -0,0 +1,32 @@
+package singularity_main
+
+import (
+	"fmt"
+
+	"github.com/JustAnotherOrganization/singularity"
+)
+
+// This example shows the basic usage for deploying a bot using singularity.
+func Example_main() {
+	s := singularity.NewSingularity()
+
+	team := s.NewTeam("xoxb-slackToken")
+
+	//Example of registering an event handler
+	team.RegisterHandler("message", func(message singularity.Message, team *singularity.SlackInstance) {
+		if message.User != "" {
+			user := team.GetUserByID(message.User)
+			if message.SubType != "message_deleted" { //If it isn't deleted.
+				fmt.Printf("%v said %v\n", user.Name, message.Text)
+			} else {
+				fmt.Printf("%v deleted %v\n", user.Name, message.Text)
+			}
+		}
+	})
+
+	if err := team.Start(); err != nil {
+		panic(err)
+	}
+
+	s.WaitForShutdown()
+}
