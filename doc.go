53f564996eeaa63592d838c0d7c8b26be60bcfc4
GrimTheReaper <megaskyhawk@gmail.com>
2016-11-03 10:40:12 -0500

    Revised

--- doc.go
+++ doc.go
@@ -1,5 +1,4 @@
-// Package Singularity
-// A Simple Slack Client/Framework in golang. Allows you to easily hook up
+// Package Singularity A Simple Slack Client/Framework in golang. Allows you to easily hook up
 // commands and event listeners, or will at some point. Right now it is in
 // progress! Nothing really here but the writings of a mad man.
 package singularity
