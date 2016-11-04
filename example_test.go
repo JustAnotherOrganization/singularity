f5ed203ced34a384d96562e45288f0b601094f26
GrimTheReaper <megaskyhawk@gmail.com>
2016-11-04 10:20:14 -0500

    Better Examples

--- example_test.go
+++ example_test.go
@@ -2,27 +2,33 @@ package singularity_test
 
 import "github.com/JustAnotherOrganization/singularity"
 
+var team singularity.SlackInstance
+
 // This example shows the basic usage for deploying a bot using singularity.
-func Example_use() {
+func Example_basic() {
 	s := singularity.NewSingularity()
 
 	team := s.NewTeam("xoxb-slackToken")
-
-	//Example of registering an event handler
-	team.RegisterHandler("message", func(message singularity.Message, team *singularity.SlackInstance) {
-		if message.User != "" {
-			user := team.GetUserByID(message.User)
-			if message.SubType != "message_deleted" { //If it isn't deleted.
-				team.Log(singularity.LogInfo, "%v said %v", user.Name, message.Text)
-			} else {
-				team.Log(singularity.LogInfo, "%v deleted %v", user.Name, message.Text)
-			}
-		}
-	})
 
 	if err := team.Start(); err != nil {
 		panic(err)
 	}
 
 	s.WaitForShutdown()
+}
+
+// This example shows how to register a basic handler.
+func Example_register_handler() {
+	team.RegisterHandler("message", func(message singularity.Message, team *singularity.SlackInstance) {
+		if message.SubType != "message_deleted" && message.User != "" {
+			team.Log(singularity.LogInfo, "%v said %v", team.GetUserByID(message.User).Name, message.Text)
+		}
+	})
+}
+
+// This example shows how to register a basic command.
+func Example_register_command() {
+	team.RegisterCommand("ping", func(command singularity.Command) {
+		command.Instance.SendMessage(singularity.Message{Text: "Pong!", Channel: command.Channel.ID})
+	})
 }
